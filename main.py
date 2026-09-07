6aae8e228032e75c5b2785557d82327df38c09a9
sumiayahpritha-web <sumiaya.h.pritha.com>
2026-09-07 19:13:29 +0600

    Added security event log

--- main.py
+++ main.py
@@ -11,10 +11,20 @@ if failed_attempts >= 5:
     print("Possible brute force attack detected")
 else:
     print("No brute force attack detected")
-username = input("Enter name: ")
-ip_address = input("Ip address: ")
-failed_attempts = int(input("Enter Failed attempts : "))
-    
+
+
+with open("events.txt","r") as file:
+    events = file.readlines()
+    print(events)
+for event in events:
+    username, ip_address, failed_attempts = event.strip().split(",")
+    failed_attempts = int(failed_attempts)
+    print("username:", username)
+    print("IP:", ip_address)
+    print("Failed attempts:", failed_attempts)
+
+
+
 
 
 
